36eb8d15e92baeb9071444d2f8180518361fd268
prithi jessica <prithijessica05@gmail.com>
2025-06-08 23:00:51 +0530

    your commit message

---
+++ vs.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, send_file, render_template, jsonify, url_for
+from PIL import Image
+from stegano import lsb
+import io
+
+app = Flask(__name__)
+
+# Landing Page
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+# Home Page (Encryption/Decryption Tool)
+@app.route('/tool')
+def home():
+    return render_template('vs.html')
+
+# Encrypt Route
+@app.route('/encrypt', methods=['POST'])
+def encrypt():
+    image_file = request.files['image']
+    message = request.form['message']
+
+    # Save the uploaded image temporarily
+    image = Image.open(image_file)
+    image_path = "original_image.png"
+    image.save(image_path)
+
+    # Hide the message in the image
+    secret_image = lsb.hide(image_path, message)
+    secret_image_path = "secret_image.png"
+    secret_image.save(secret_image_path)
+
+    # Send the encoded image for download
+    return send_file(secret_image_path, mimetype='image/png', as_attachment=True, download_name='encrypted_image.png')
+
+# Decrypt Route
+@app.route('/decrypt', methods=['POST'])
+def decrypt():
+    image_file = request.files['image']
+
+    # Save the uploaded image temporarily
+    image = Image.open(image_file)
+    image_path = "uploaded_image.png"
+    image.save(image_path)
+
+    # Reveal the hidden message
+    hidden_message = lsb.reveal(image_path)
+
+    return jsonify({'message': hidden_message})
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
